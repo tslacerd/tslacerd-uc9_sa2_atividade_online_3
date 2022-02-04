6546c6d677d1b5c54d5bc465e388b238da3332c8
tslacerd <tslacerd@gmail.com>
2022-02-04 16:02:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function mostrarPopUp() {
+    window.alert("Venha para nosso time!!!")
+}
+
+function cadastrarEmail() {
+    let email = document.getElementById ("campo email").value;
+    console.log (email);
+}
+
